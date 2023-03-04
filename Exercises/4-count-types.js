0011416b1f5eb99351be92c1ca80997e4da72335
andreykurochkin <andrey.kurochkin@hotmail.com>
2023-03-04 22:11:33 +0300

    Add fourth task

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr) => {
+  const hash = {};
+  for(const item of arr) {
+    const key = typeof(item);
+    const value = hash.hasOwnProperty(key) ? ++hash[key] : 1;
+    hash[key] = value;
+  }
+  return hash;
+};
 
 module.exports = { countTypesInArray };
